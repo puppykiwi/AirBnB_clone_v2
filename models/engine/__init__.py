731a393fa7ca332ef37ddb79a9ef6becd933ab77
puppykiwi <johnraymwendwa04@gmail.com>
2023-06-21 12:02:21 +0300

    hb nb

--- models/engine/__init__.py
+++ models/engine/__init__.py
@@ -2,3 +2,15 @@
 '''
     Package initializer
 '''
+from os import getenv
+from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+
+if HBNB_TYPE_STORAGE == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
