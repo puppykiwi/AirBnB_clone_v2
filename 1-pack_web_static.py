bc1d5def8025683a05c4916e30ba74cb850f080d
puppykiwi <johnraymwendwa04@gmail.com>
2023-07-07 11:38:23 +0300

    else

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -13,8 +13,10 @@ def do_pack():
     if path.isdir("versions") is False:
         if local("mkdir -p versions").failed is True:
             return None
+
     if local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
-    return file
+    else:
+        return file
 
 do_pack()
