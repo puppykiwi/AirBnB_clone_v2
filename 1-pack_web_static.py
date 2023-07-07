16d5594b52a2a09f8fdadc42a23a7262384cf1c7
puppykiwi <johnraymwendwa04@gmail.com>
2023-07-07 11:36:09 +0300

    task1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# a fabfile that generates a .tgz archive from the contents of the web_static
+
+from fabric.api import local
+from datetime import datetime
+from os import path
+
+def do_pack():
+    """generates a .tgz archive from the contents of the web_static folder"""
+    date = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(date.year, date.month, date.day, date.hour, date.minute, date.second)
+
+    if path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
+        return None
+    return file
+
+do_pack()
