84c986f0f07b070741fa3e30df19b7d5d63e8e22
puppykiwi <johnraymwendwa04@gmail.com>
2023-07-07 11:51:05 +0300

    huuuh

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,37 +1,22 @@
 #!/usr/bin/python3
-"""script that generates a .tgz archive from the contents of the web_static
-folder of your AirBnB Clone repo, using the function do_pack.
-"""
-from datetime import datetime
-from fabric.api import local
-import os
+"""a fabfile that generates a .tgz archive from the contents of the web_static folder"""
 
+from fabric.api import local
+from datetime import datetime
+from os import path
 
 def do_pack():
-    """ generates a .tgz archive from the contents of the web_static
+    """generates a .tgz archive from the contents of the web_static folder"""
+    date = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(date.year, date.month, date.day, date.hour, date.minute, date.second)
 
-    All files in the folder web_static must be added to the final archive.
-    All archives must be stored in the folder versions.
-    The name of the archive created must be:
-        web_static_<year><month><day><hour><minute><second>.tgz
-    The function do_pack must return the archive path if the archive has
-    been correctly generated. Otherwise, it should return None.
+    if path.isdir("versions") is False:
+        local("mkdir -p versions")
 
-    Returns:
-        fabric.operations._AttributeString: archive path.
-    """
-    now = datetime.now().strftime("%Y%m%d%H%M%S")
-
-    # create folder versions if it doesn’t exist
-    local("mkdir -p versions")
-
-    # extract the contents of a tar archive
-    result = local("tar -czvf versions/web_static_{}.tgz web_static"
-                   .format(now))
-    if result.failed:
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
     else:
-        return result
+        return file
 
 if __name__ == "__main__":
     do_pack()
