7aa2e9a53c833190ca1995b8fa5d7f807bdc2519
puppykiwi <johnraymwendwa04@gmail.com>
2023-08-07 11:51:32 +0300

    .

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,10 +11,12 @@ def hello():
     """ returns Hello HBNB! to the route / """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ returns HBNB to the route /hbnb """
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
