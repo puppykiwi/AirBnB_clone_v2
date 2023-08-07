eeeedd1d205a51abe97290e7bd5ec48ec7de8a79
puppykiwi <johnraymwendwa04@gmail.com>
2023-08-07 11:48:13 +0300

    bump

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ Script that starts a Flask web application """
 
-from __init__ import app
+from flask import Flask
+
+app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
