bfab0603905879b1be4038dc062c025a81ba36c3
puppykiwi <johnraymwendwa04@gmail.com>
2023-08-07 11:44:36 +0300

    exec

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,11 +3,12 @@
 
 from __init__ import app
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """ Hello HBNB! """
     return 'Hello HBNB!'
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
