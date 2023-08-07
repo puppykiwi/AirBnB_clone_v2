9eedcbcfc189af6e6cae97643e6de91996d0c455
puppykiwi <johnraymwendwa04@gmail.com>
2023-08-07 13:59:31 +0300

    see

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,33 +1,24 @@
 #!/usr/bin/python3
 """script that starts a Flask web application"""
+from flask import Flask, render_template, g
+import models
+from models.state import State
 
 
-# import Flask class from flask module
-# import render_template for rendering templates to browser
-# fetch data from storage engine
-from flask import Flask, render_template
-import sys
-sys.path.append("/opt/AirBnB_clone_v2")
-from models import storage
+app = Flask(__name__, template_folder='templates')
 
-# create an instance called app of the class by passong the __name__ variable
-app = Flask(__name__)
-app.url_map.strict_slashes = False
+
+@app.route('/states_list', strict_slashes=False)
+def list_states():
+    """view that lists all of the states"""
+    states = models.storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown_db(exception=None):
-    """removes the current SQLAlchemy Session
-    """
-    if storage is not None:
-        storage.close()
-
-
-@app.route('/states_list')
-def states_list(n=None):
-    """displays a HTML page: inside the tag BODY"""
-    states = storage.all('State')
-    return render_template('7-states_list.html', states=states)
+def tear_down(error):
+    """remove the current SQLAlchemy Session"""
+    models.storage.close()
 
 
 if __name__ == '__main__':
